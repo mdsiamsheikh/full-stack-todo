2974cbb58fb67079088683410ef586f9fed3b951
mdsiamsheikh <mdsiamsheikh6640@gmail.com>
2023-10-11 07:44:26 +0600

    fix this url

--- components/Todos.vue
+++ components/Todos.vue
@@ -11,21 +11,20 @@ onMounted(() => {
 <template>
   <!-- component -->
   <div class="flex items-center justify-center h-screen">
-    <div class="w-11/12 p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+    <div
+      class="w-11/12 p-6 m-4 bg-white rounded rounded-lg shadow lg:w-3/4 lg:max-w-lg"
+    >
       <div
         class="flex items-center justify-center p-3 border-2 rounded-full m-14"
       >
         <h1 class="text-xl font-semibold">Full-Stack ToDo</h1>
       </div>
-
       <div class="mb-4">
         <div class="flex mt-4">
-          <label for="todoInput" class="sr-only">Todo Item</label>
           <input
             type="text"
-            id="todoInput"
-            class="w-full px-3 py-2 mr-4 border rounded shadow appearance-none text-grey-darker"
             placeholder="Insert your todo"
+            class="w-full px-3 py-2 mr-4 border rounded shadow appearance-none text-grey-darker"
             v-model="store.newTodo"
           />
           <button
@@ -36,43 +35,54 @@ onMounted(() => {
             Submit
           </button>
           <button
-            v-if="store.addButton"
+            v-if="store.updateButton"
             class="p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-red-500"
-            @click="store.addTodo()"
+            @click="store.updateTodo()"
           >
             Update
           </button>
         </div>
+        <p class="justify-center m-2 text-xs text-center text-red-600">
+          {{ store.errMsg }}
+        </p>
       </div>
       <ul>
-        <li class="mb-4 border">
-          <div class="flex items-center justify-between p-1 px-4 text-center">
-            <div><h2 class="text-lg font-semibold">Task 1</h2></div>
-            <div>
-              <button class="p-2 rounded hover:text-white hover:bg-red-500">
-                Edit
-              </button>
-              <button
-                class="p-2 ml-2 text-red-500 border-red-500 rounded hover:text-white hover:bg-red-500"
-              >
-                Delete
-              </button>
+        <li>
+          <div class="p-3 mt-4 border rounded-lg border-white-600">
+            <div class="list-text">
+              <div><h2 class="text-lg font-semibold">Task 1</h2></div>
             </div>
-          </div>
-        </li>
-
-        <li class="mb-4 border">
-          <div class="flex items-center justify-between p-1 px-4 text-center">
-            <div><h2 class="text-lg font-semibold">Task 2</h2></div>
-            <div>
-              <button class="p-2 rounded hover:text-white hover:bg-red-500">
-                Edit
-              </button>
-              <button
-                class="p-2 ml-2 text-red-500 border-red-500 rounded hover:text-white hover:bg-red-500"
+            <div
+              class="todo-list"
+              v-for="todo in computedTodos"
+              :key="todo._id"
+            >
+              <div
+                class="flex items-center justify-between p-2 mt-3 border rounded-lg border-white-500"
               >
-                Delete
-              </button>
+                <div class="items">
+                  <p
+                    class="font-sans text-lg font-bold font-xl text-slate-500"
+                    :style="{ color: todo.color }"
+                  >
+                    {{ todo.todo }}
+                  </p>
+                </div>
+                <div class="button">
+                  <button
+                    class="p-2 ml-2 text-red-500 border-red-500 rounded hover:text-white hover:bg-red-500"
+                    @click="store.editTodo(todo._id, todo)"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    class="p-2 ml-2 text-red-500 border-red-500 rounded hover:text-white hover:bg-red-500"
+                    @click="store.deleteTodo(todo._id)"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </li>
