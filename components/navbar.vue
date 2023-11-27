c9b6f64ad9ba5b8fec6ee9fd237f634fa558e89d
mdsiamsheikh <mdsiamsheikh6640@gmail.com>
2023-11-28 00:22:33 +0600

    add logout page name and email

--- components/navbar.vue
+++ components/navbar.vue
@@ -33,26 +33,15 @@
             v-show="showPopupProfileBox"
             class="block popup absolute bg-gray-100 w-[250px] min-h-[155px] top-[2.5rem] right-0 bottom-0 z-10 rounded-lg shadow-lg"
           >
+            <p>{{ store.user.displayName }}</p>
+            <div>
+              <h3>{{ store.user.email }}</h3>
+            </div>
             <button
-              class="flex justify-center text-center border-solid border-2 border-indigo-200 gap-2 px-3 py-1.5 border-t mt-6"
+              class="text-center border-solid border-2 bg-[#A555EC] border-indigo-200 gap-2 px-3 py-1.5 border-t mt-6"
               @click="logout"
             >
-              <div
-                class="flex justify-center items-center p-1 bg-[#863BF7] rounded-full"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="rounded-full"
-                  height="20"
-                  width="20"
-                  fill="#fff"
-                >
-                  <path
-                    d="M4.5 17q-.625 0-1.062-.438Q3 16.125 3 15.5v-11q0-.625.438-1.062Q3.875 3 4.5 3H10v1.5H4.5v11H10V17Zm9-3.5-1.062-1.062 1.687-1.688H8v-1.5h6.125l-1.687-1.688L13.5 6.5 17 10Z"
-                  />
-                </svg>
-              </div>
-              <NuxtLink class="text-lg text-[#127582]">Log Out</NuxtLink>
+              <NuxtLink class="text-lg text-black">Log Out</NuxtLink>
             </button>
           </ProfilePopup>
         </div>
